3ee01eeb439bfc8cd3913280c037c8a6e1c516da
Amy Moretti <amyemoretti@gmail.com>
2018-09-11 16:38:31 -0500

    html updates

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Defining requirements
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // Set Up Express Server
 var app = express();
@@ -10,6 +11,18 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Connect with HTML
+app.use(express.static(path.join(__dirname, '/app/public'), {
+  index: false,
+  extensions: ['html']
+}));
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {
+      root: __dirname + "/app/public"
+  });
+});
 
 
 // Start Server
